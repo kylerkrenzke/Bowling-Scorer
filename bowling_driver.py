d2576359d728a97a4ade0a76c49772c6fa7844ba
Kyler <kkrenzke@gmail.com>
2020-10-01 14:27:17 -0500

    wip

--- bowling_driver.py
+++
@@ -1,6 +0,0 @@
-# Driver for bowling_score.py
-# 
-# Author: Kyler Krenzke
-
-from bowling_scorer import *
-
--- bowling_scorer.py
+++ bowling_scorer.py
@@ -1,16 +1,25 @@
 class BowlingGame:
-    def __init__(self, nplayers):
-        self.nplayers = nplayers
-        self.scores = [[(None, None) for j in range(10)] for i in range(nplayers)]
+    def __init__(self, names):
+        self.names = names
+        self.nplayers = len(names)
+        self.scores = [[(None, None) for j in range(10)] for i in range(self.nplayers)]
+        self.totals = [[None for j in range(10)] for i in range(self.nplayers)]
         
     def setFrameScore(self, player, frame, score):
         self.scores[player][frame] = score
         
+    def isTerminal(self):
+        for player in self.scores:
+            for frame in player:
+                if frame[0:1] == (None, None):
+                    return False
+        return True
+        
 def formatFrame(frame):
-    if frame == (None, None):
+    if frame[0] == frame[1] == None:
         return "|   |"
-    elif frame == (10, None):
-        return "|  X|"
+    elif isinstance(frame[0], int) and frame[1] == None:
+        return "|  X|" if frame[0] == 10 else "|{}  |".format(frame[0])
     else:
         return "|{0} {1}|".format(frame[0], '/' if frame[0]+frame[1]==10 else frame[1])
         
@@ -20,34 +29,40 @@ def formatFrameList(frame_lst):
         ret = ret + formatFrame(lst)
     return ret
     
-def formatPlayerList(player_lst):
+def formatPlayerList(player_lst, names):
     ret = ""
-    for i in range(5 * 10):
+    for i in range(5*10 + 12):
         ret += '-'
     ret += '\n'
-    for lst in player_lst:
-        ret = ret + formatFrameList(lst) + '\n'
-    for i in range(5 * 10):
+    for i in range(len(player_lst)):
+        ret += "|{0}|".format(names[i].ljust(10))
+        ret += formatFrameList(player_lst[i]) + '\n'
+    for i in range(5*10 + 12):
         ret += '-'
     return ret
         
-def printGame(game):
-    for player in range(game.nplayers):
-        for i in range(10):
-            if game.scores[player][i] == (None, None):
-                print(' ')
-            elif game.scores[player][i] == (10,None):
-                print('X')
-            elif game.scores[player][i][0] + game.scores[player][i][1] == 10:
-                print(game.scores[player][i][0],'/')
-            else:
-                print(game.scores[player][i][0],game.scores[player][i][1])
+def formatGame(game):
+    return "Scoreboard:\n{0}".format(formatPlayerList(game.scores, game.names))
         
+def getPlayers():
+    print("Enter player names 1 at a time (between 2-4). Enter 'done' when finished\n")
+    name = ""
+    names = []
+    while name != "done" and len(names) != 4:
+        name = input(">> ")
+        if len(name) > 10:
+            name = name[0:10]
+        if name != "done":
+            names.append(name)
+    return names
+
 print("----- Bowling Scorer -----\n\nAuthor: Kyler Krenzke\n")
-nplayers = int(input("How many players? "))
-while nplayers < 2 or nplayers > 4:
-    nplayers = int(input("Please enter a number between 2 and 4. How many players? "))
     
-game = BowlingGame(nplayers)
+names = getPlayers()
+game = BowlingGame(names)
 game.setFrameScore(0, 0, (10, None))
-print(formatPlayerList(game.scores))
+game.setFrameScore(0, 1, (9, None))
+game.setFrameScore(0, 2, (8, 2))
+game.setFrameScore(0, 3, (7, 2))
+
+print(formatGame(game))
